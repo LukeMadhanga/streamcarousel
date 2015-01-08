f6990b0050cb705fe6f62b210eae1ee20b96a31c
LukeMadhanga <lmadhanga@gmail.com>
2015-01-08 09:47:26 +0000

    Removed premature optimisation code

--- streamcarousel.js
+++ streamcarousel.js
@@ -2,28 +2,29 @@
 /**
  * The StreamCarousel closure
  * @param {object(jQuery)} $ The jQuery object
- * @param {object(Window)} w The window object
- * @param {int} c Count of this particular carousel
+ * @param {object(Window)} window The window object
+ * @param {int} count Count of this particular carousel
+ * @param {object(Math)} Math Reference to the Math object
  */
-function ($,w,c){
+function ($, window, count, Math){
     /**
      * The stream carousel object
      * @syntax $(selector).streamCarousel({...})
-     * @param {objetc(plain)} o Options for this object
+     * @param {objetc(plain)} options Options for this object
      * @returns {object(jQuery).fn.streamCarousel} The jQuery object for the selected ul, allowing for chainability
      */
-    $.fn.streamCarousel = function (o) {
+    $.fn.streamCarousel = function (options) {
         var t = $(this),
         T = this,
         tp = t.parent();
         if (t.length > 1) {
             t.each(function () {
                 // Apply this function to all of the elements that the selector matches
-                $(this).streamCarousel(o);
+                $(this).streamCarousel(options);
             });
             return this;
         }
-        T.c = c;
+        T.c = count;
         var e = 0,
         li = t.find('li'),
         s = $.extend({
@@ -34,45 +35,37 @@ function ($,w,c){
             prefix: 'streamcarousel', 
             random: !1, 
             transitionSpeed: 750
-        }, o),
+        }, options),
         // Shorcuts to access jQuery methods that are used a lot
-        rc = 'removeClass',
-        ac = 'addClass',
-        r = 'replace',
-        cl = 'click',
-        le = 'length',
-        si = 'strcarint',
-        d = 'data',
-        m = Math,
-        mes = '[data-strcarid="'+T.c+'"]',
         pa = !1,
-        tr = 'opacity ' + s.transitionSpeed + 'ms ease-in-out';
-        t[ac]('streamcarousel');
-        li.css({'-webkit-transition': tr, '-moz-transition': tr, transition: tr});
-        li[ac]('scOld');
-        li.eq(e)[rc]('scOld')[ac]('scCur');
-        tp[ac]('scparent').attr({'data-strcarid':T.c});
+        transition = 'opacity ' + s.transitionSpeed + 'ms ease-in-out';
+        t.addClass('streamcarousel');
+        li.css({'-webkit-transition': transition, '-moz-transition': transition, transition: transition});
+        li.addClass('scOld');
+        li.eq(e).removeClass('scOld').addClass('scCur');
+        tp.addClass('scparent').attr({'data-strcarid':T.c});
         if (s.descriptions){
-            tp.append('<£ %"scdhold"><£ %"scdesc"><a %"scdtitle"></a><a %"scdtext"></a></£><£ %"sccon"><£ %"scl"></£>&<£ %"scr"></£></£></£>'[r](/&/g, s.auto?'<£ %"scpa"></£>':'')[r](/£/g,'div')[r](/%/g,'class='));
+            tp.append('<div class="scdhold">' + 
+                        '<div class="scdesc"><a class="scdtitle"></a><a class="scdtext"></a></div><div class="sccon"><div class="scl"></div>&<div class="scr"></div></div></div>'.replace(/&/g, s.auto?'<div class="scpa"></div>':''));
             if(li.length===1){
                 $('.scl,.scr', tp).css({display:'none'});
             }
         }
         T.n = function() {
             // Go to next
-            e = s.random ? m.floor(m.random()*(li[le]-(0+1))+0) : (e + 1 === li[le] ? 0 : e+1);
+            e = s.random ? Math.floor(Math.random()*(li.length-(0+1))+0) : (e + 1 === li.length ? 0 : e+1);
             T.tog();
         };
         T.p = function() {
             // Go to previous
-            e = s.random ? m.floor(m.random()*(li[le]-(0+1))+0) : (e - 1 < 0 ? li[le]-1 : e-1);
+            e = s.random ? Math.floor(Math.random()*(li.length-(0+1))+0) : (e - 1 < 0 ? li.length-1 : e-1);
             T.tog();
             T.ri();
         };
         T.tog=function(){
             // Toggle the image that is in view and change the description text
-            $('.scCur', tp)[rc]('scCur')[ac]('scOld');
-            li.eq(e)[rc]('scOld')[ac]('scCur');
+            $('.scCur', tp).removeClass('scCur').addClass('scOld');
+            li.eq(e).removeClass('scOld').addClass('scCur');
             if (s.descriptions) {
                 T.sd(e);
             }
@@ -80,40 +73,40 @@ function ($,w,c){
         T.ri=function(){
             // Reset the interval
             if (!pa && s.auto) {
-                w.clearInterval(w[si+T.c]);
-                w[si+T.c] = w.setInterval(T.n, s.pause);
+                window.clearInterval(window['strcarint'+T.c]);
+                window['strcarint'+T.c] = window.setInterval(T.n, s.pause);
             }
         };
         T.sd = function(e) {
             // Set the description text
             var im = li.eq(e).find('img');
-            $('.scdtitle', tp).html(im[d]('title'));
-            $('.scdtext', tp).html(im[d]('description'));
+            $('.scdtitle', tp).html(im.data('title'));
+            $('.scdtext', tp).html(im.data('description'));
         };
         T.sd(e);
         if (s.auto) {
             // If we are automatic, start the changing
-            w[si+T.c] = w.setInterval(T.n, s.pause);
+            window['strcarint'+T.c] = window.setInterval(T.n, s.pause);
         }
         // When the left arrow is clicked, go to the previous image (looping)
-        $(mes+' .scl').unbind(cl)[cl](function () {T.p();});
-        $(mes+' .scpa').unbind(cl)[cl](function () {
+        $('[data-strcarid="'+T.c+'"] .scl').unbind('click').click(function () {T.p();});
+        $('[data-strcarid="'+T.c+'"] .scpa').unbind('click').click(function () {
             // What to do when the pause/play button is clicked
             var ti = $(this);
             if(ti.hasClass('scpl')) {
                 pa=!1;
                 T.ri();
-                ti[rc]('scpl');
+                ti.removeClass('scpl');
             } else {
                 pa=!0;
-                w.clearInterval(w[si+T.c]);
-                ti[ac]('scpl');
+                window.clearInterval(window['strcarint'+T.c]);
+                ti.addClass('scpl');
             }
         });
         // When the right arrow is clicked, go to the next image (looping)
-        $(mes+' .scr').unbind(cl)[cl](function () {T.n();T.ri();});
-        c++;
+        $('[data-strcarid="'+T.c+'"] .scr').unbind('click').click(function () {T.n();T.ri();});
+        count++;
         return T;
     };
 
-})(jQuery, this, 0);
+})(jQuery, this, 0, Math);
